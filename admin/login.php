0fcd5c51dee7aaab22b686f93535bc704b764926
HolitSky <holit.singkawang12@gmail.com>
2025-11-14 13:14:22 +0700

    tes

--- admin/login.php
+++ admin/login.php
@@ -389,7 +389,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             </div>
             
             <button type="submit" class="btn btn-primary btn-login w-100">
-                <i class="bi bi-box-arrow-in-right"></i> Login
+                <i class="bi bi-box-arrow-in-right"></i> Logins
             </button>
         </form>
         
